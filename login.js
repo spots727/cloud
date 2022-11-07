4cd107e2e33086ac0fac4d9a1c14a93f870d2923
spots727 <81703554+spots727@users.noreply.github.com>
2022-11-07 20:54:25 +0100

    s

--- login.js
+++
@@ -1,9 +0,0 @@
-function login(){
-    setCookie("username", username, 186)
-    setCookie("password", password, 186)
-    if(checkCookie()){
-        window.location.href = "/cloud/home.html"
-    } else{
-        window.location.reload()
-    }
-}
